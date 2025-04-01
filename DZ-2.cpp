a60e586d36bdb9003b342d0df678c5946330b9f9
Alex LG545 <antitroll545@mail.ru>
2025-04-01 23:46:10 +0300

    DZ-2

---
+++ DZ-2.cpp
@@ -0,0 +1,44 @@
+﻿// DZ-2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+
+
+#include <iostream>
+#include <string>
+
+struct Balance {
+	std::string name;
+	int id;
+	int balance;
+
+
+
+};
+void newBalance(Balance& person);
+
+
+int main() {
+	Balance personOne{ "Ivan", 22222, 6000 };
+	newBalance(personOne);
+
+	std::cout << personOne.name << '\t' << personOne.id << '\t' << personOne.balance << '\n';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+void newBalance(Balance& person) {
+
+	std::cout << "Enter new balance:" << std::endl;
+	std::cin >> person.balance;
+
+};
