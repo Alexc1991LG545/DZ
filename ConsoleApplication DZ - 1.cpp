dbece2c6746c9963ad0bcb8898e24d684b120f8a
Alex LG545 <antitroll545@mail.ru>
2025-04-01 23:41:18 +0300

    DZ-1

---
+++ ConsoleApplication DZ - 1.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+
+enum Months {
+    january = 1, february
+    , march, april, may, june, julay, august, september, october, november, december
+};
+Months getCurrentMonth();
+
+int main()
+{
+    Months month{ getCurrentMonth() };
+    
+
+
+
+
+
+
+
+
+}
+
+Months getCurrentMonth() {
+    Months currentMonth{};
+
+    int month{};
+    std::cin >> month;
+
+    
+    currentMonth = static_cast<Months>(month);
+    std::cout << currentMonth << std::endl;
+    return currentMonth;
+   
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
