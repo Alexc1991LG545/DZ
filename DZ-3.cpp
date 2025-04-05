cf1c4114c200f77cacb35870b551a4efb8dc1795
Alex LG545 <antitroll545@mail.ru>
2025-04-05 11:52:36 +0300

    new rewrk added DZ-3

---
+++ DZ-3.cpp
@@ -0,0 +1,33 @@
+﻿// DZ-3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+
+struct City {
+    std::string city{};
+
+    std::string street{};
+    int house{};
+
+    int flat{};
+    int index{};
+
+    
+
+};
+
+void putOnTable(City& structure) 
+{
+    std::cout << structure.city << '\t' << structure.street << '\t' << structure.house << '\t' << structure.flat << '\t' << structure.index << std::endl;
+}
+
+
+int main()
+{
+    City One{ "Moscow", "Krasnaya Ploshad",79,545, 2456 };
+    City Two{ "Kaluga", "Lenina", 54, 789, 5656 };
+
+    putOnTable(One);
+    putOnTable(Two);
+}
