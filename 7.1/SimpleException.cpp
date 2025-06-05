ac49346017a68f52ebe613042aa05709da6b45ba
Alex LG545 <antitroll545@mail.ru>
2025-06-05 23:49:20 +0300

    class 7.1

---
+++ 7.1/SimpleException.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+#include "MyException.h"
+
+int function(std::string str, int forbidden_length) {
+	int length = str.length();
+	if (length == forbidden_length) throw MyException("Вы ввели слово запретной длинны! До свидания\n");
+	return length;
+}
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::cout << "Введите запретную длину: ";
+	int len{};
+	std::cin >> len;
+	while (1) {
+		std::cout << "Введите слово: ";
+		std::string word;
+		std::cin >> word;
+		try {
+			std::cout << "Длина слова \"" << word << "\" равна " << function(word, len) << std::endl;
+		}
+		catch (MyException myExcp) {
+			std::cout << myExcp.what() << std::endl;
+			break;
+		}
+	}
+}
