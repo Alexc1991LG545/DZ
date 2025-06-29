a9c69d263253dad4a0546e46a69c2e3215bf42c5
Alex LG545 <antitroll545@mail.ru>
2025-06-29 06:01:34 +0300

    5.2.2

--- 5.2.2/smartArrayRaii.cpp
+++ 5.2.2/smartArrayRaii.cpp
@@ -54,9 +54,12 @@ int main()
 	arr.add_element(1);
 	arr.add_element(4);
 	arr.add_element(155);
-	arr.add_element(14);
-	arr.add_element(15);
-	std::cout << arr.get_element(1) << std::endl;
+
+	smart_array new_array(2);
+	new_array.add_element(44);
+	new_array.add_element(34);
+	
+	arr = new_array;		
   }
   catch (const std::exception& ex) {
 	std::cout << ex.what() << std::endl;
