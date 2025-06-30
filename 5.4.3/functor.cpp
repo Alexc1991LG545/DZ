a019f23885223bb7e034376e67f344c86a94a714
Alex LG545 <antitroll545@mail.ru>
2025-06-30 09:01:47 +0300

    5.4.3

---
+++ 5.4.3/functor.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+class Functor {
+  unsigned m_sum;
+  unsigned m_counter; 
+public:
+  Functor() : m_sum{ 0 }, m_counter{ 0 } {   
+  }
+  void operator()(int k) { 
+    if (k % 3 == 0) {
+      m_sum += k;
+      ++m_counter;
+    }
+  }
+  unsigned get_sum() {
+    return m_sum;
+  }
+  unsigned get_count() {
+    return m_counter;
+  }
+};
+int main() {
+  Functor f;
+  f( 4 );
+  f( 1 );
+  f( 3 );
+  f( 6 );
+  f( 25 );
+  f( 54 );
+  std::cout << "get_sum() = " << f.get_sum() << std::endl;
+  std::cout << "get_count() = " << f.get_count() << std::endl;
+  return 0;
+}
