f53ba45e260e3067c4638d508e5000d96c8a0adb
Alex LG545 <antitroll545@mail.ru>
2025-07-01 01:42:59 +0300

    5.5.1

---
+++ 5.5.1/count_simb.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <set>
+#include <map>
+#include <unordered_set>
+
+int main()
+{
+  std::string str{ "Hello world!!" };
+  std::multiset<char> mess{ str.begin(), str.end()};
+  std::unordered_set<char> unMess{ mess.begin(), mess.end()};
+  std::multimap<int, char, std::greater<int>> res;
+
+  for (const auto& k : unMess) {
+    res.insert(std::make_pair(mess.count(k), k));
+  }
+
+  for (const auto& k : res) {
+    std::cout << k.second << ": " << k.first << std::endl;
+  }
+
+  return 0;
+}
